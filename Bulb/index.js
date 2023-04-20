60249d720e18b798f32403bedc54e69d3cb63935
Iva Havranová <havranova.i@seznam.cz>
2023-04-20 19:44:08 +0200

    Done

---
+++ Bulb/index.js
@@ -0,0 +1,19 @@
+export const Bulb = (props) => {
+  const { isOn } = props;
+  const element = document.createElement('div');
+  element.classList.add('bulb');
+
+  if (isOn) {
+    element.classList.add('bulb--on');
+  }
+
+  element.addEventListener('click', () => {
+    element.replaceWith(
+      Bulb({
+        isOn: !isOn,
+      }),
+    );
+  });
+
+  return element;
+};
--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+import { Bulb } from './Bulb/index.js';
+
 console.log('funguju!');
+
+const appElm = document.querySelector('#app');
+
+appElm.append(
+  Bulb({ isOn: true }),
+  Bulb({ isOn: false }),
+  Bulb({ isOn: true }),
+);
